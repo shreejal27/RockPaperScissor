b84604bb03955bf60d0a3189bf8631f6bc20fcde
Shreejal Maharjan <shreejal27@gmail.com>
2022-12-26 15:43:09 +0545

    added game logic

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-function Uvalue(value){
+var u=0;
+var c=0;
+
+function startGame(value){
     var user_choice= value;
     user(user_choice);
-    Cvalue();
-
+    cvalue();
+    gameLogic(u,c);
 }
 
 function user(user_choice){
     var userBox= document.getElementById("user_box");
     if(user_choice == "1"){
-        userBox.innerHTML = "<img src='icons/rock-modified.png'  width='300px' height='300px'>";
+        userBox.innerHTML = "<img src='icons/rock-modified.png'  width='200px' height='200px'>";
+        u=1;
     }
     else if(user_choice == "2"){
-    userBox.innerHTML = "<img src='icons/paper-modified.png'  width='300px' height='300px'>";
+         userBox.innerHTML = "<img src='icons/paper-modified.png'  width='200px' height='200px'>";
+         u=2;
     }
     else{
-    userBox.innerHTML = "<img src='icons/scissor-modified.png'  width='300px' height='300px'>";
+         userBox.innerHTML = "<img src='icons/scissor-modified.png'  width='200px' height='200px'>";
+         u=3;
     }
 }
 
-function Cvalue(){
+function cvalue(){
     var computer_choice = Math.floor(Math.random()*3)+1;
     computer(computer_choice);
 }
@@ -27,16 +33,57 @@ function computer(computer_choice){
     var computerbox= document.getElementById("computer_box");
 
     if(computer_choice == 1){
-        computerbox.innerHTML = "<img src='icons/rock-modified.png'  width='300px' height='300px'>";
+        computerbox.innerHTML = "<img src='icons/rock-modified.png'  width='200px' height='200px'>";
+         c=1;
     }
    else if(computer_choice == 2){
-    computerbox.innerHTML = "<img src='icons/paper-modified.png'  width='300px' height='300px'>";
+          computerbox.innerHTML = "<img src='icons/paper-modified.png'  width='200px' height='200px'>";
+          c=2;
     }
     else{
-    computerbox.innerHTML = "<img src='icons/scissor-modified.png'  width='300px' height='300px'>";
+         computerbox.innerHTML = "<img src='icons/scissor-modified.png'  width='200px' height='200px'>";
+         c=3;
     }
 }
        
-function result(){
-    
+function gameLogic(u,c){
+    const msg1= "User Wins !";
+    const msg2= "Computer Wins !"; 
+    const msg3= "Draw!!!";
+    const msg4= "!!!!! Game Crashed !!!!!";
+    var resultText = document.getElementById("resultText");
+
+    if(u==1 && c==1){
+        resultText.innerHTML= msg3;
+    }
+    else if(u==1 && c==2){
+        resultText.innerHTML= msg2;
+    }
+    else if(u==1 && c==3){
+        resultText.innerHTML= msg1;
+    }
+    else if(u==2 && c==1){
+        resultText.innerHTML= msg1;      
+    }
+    else if(u==2 && c==2){
+        resultText.innerHTML= msg3;
+    }
+    else if(u==2 && c==3){
+        resultText.innerHTML= msg2;     
+    }
+    else if(u==3 && c==1){
+        resultText.innerHTML= msg2;
+   
+    }
+    else if(u==3 && c==2){
+        resultText.innerHTML= msg1;
+        
+    }
+    else if(u==3 && c==3){
+        resultText.innerHTML= msg3;
+      
+    }
+    else{
+        resultText.innerHTML= msg4;
+    }
 }
